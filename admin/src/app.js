aa7bbe42c20734ad2ada316ee7edc2944292bc09
unknown <muh.it.eng.19841984@gmail.com>
2022-11-17 09:33:16 +0200

    #03 init admin server

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,1 +1,11 @@
-console.log("hello here");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var PORT = 8000;
+var app = express();
+app.use(cors({
+    origin: ["http://localhost:3000"],
+}));
+app.use(express.json());
+app.listen(PORT, function () { return console.log("server running on port ".concat(PORT)); });
--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,1 +1,11 @@
-console.log("hello here");
+import * as express from "express";
+import * as cors from "cors";
+const PORT = 8000;
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+app.use(express.json());
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
